3db65773544011dd98c32193f2ad785226874902
Bernardo Santiago <bernardosantiago44@gmail.com>
2025-05-26 08:14:50 -0600

    Regular expression to parse lines

--- app.R
+++ app.R
@@ -8,6 +8,18 @@ grammarToAutomaton <- function(text) {
   
 }
 
+parseGrammarLine <- function(line) {
+  matches <- regmatches(line, regexec("^([A-Z])\\s*->\\s*([a-z]*)\\s*([A-Z]?)$", line))
+  if (length(matches[[1]]) > 0) {
+    variable <- matches[[1]][2]  # "S"
+    terminal <- matches[[1]][3]  # "a"
+    next_variable <- matches[[1]][4]  # "S"
+    print(paste("Variable:", variable))
+    print(paste("Terminal:", terminal))
+    print(paste("Next Variable:", next_variable))
+  }
+}
+
 # Define UI for app that draws a histogram ----
 ui <- fluidPage(
   
